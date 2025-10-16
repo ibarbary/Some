99e6bb7d254097d49729b3d5720c728f687a94f9
Ahmed Barbary <98464590+ibarbary@users.noreply.github.com>
2025-10-16 15:23:00 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,9 +29,8 @@ app.use(globalErrorHandler);
 
 app.get("/", (req, res) => res.json({ status: "ok" }));
 
-if (process.env.NODE_ENV !== "production") {
-  const port = process.env.PORT || 5000;
-  app.listen(port, () => console.log(`Server running on port ${port}`));
-}
 
-export default app;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
